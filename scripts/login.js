84a3db5f92f5fdd7010bb713e464abc0c26bd2a0
unknown <ngocanha85@gmail.com>
2022-05-05 08:13:06 +0700

    sua lan 3

--- scripts/login.js
+++ scripts/login.js
@@ -1,5 +1,26 @@
-
+let userInput = document.getElementById("input-username")
+let passwordInput = document.getElementById("input-password")
+let submitBtn = document.getElementById("btn-submit")
 
 let userArr = getFromStorage("users").map(obj => parseUser(obj)) || []
 
 console.log("User arr: ", userArr);
+
+function validateLogin(data) {
+    if (!data.userName) {
+        alert("Please enter username!")
+    }
+    if (!data.password) {
+        alert("Please enter password!")
+    }
+}
+
+function loginData() {
+    let user = {
+        userName: userInput.value,
+        password: passwordInput.value
+    }
+    validateLogin(user)
+}
+
+submitBtn.addEventListener("click", loginData)
