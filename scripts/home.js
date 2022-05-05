6b32ccdb05694104be3a8d7c28083242f2e1681f
unknown <ngocanha85@gmail.com>
2022-05-06 00:08:00 +0700

    sua lan 4

--- scripts/home.js
+++ scripts/home.js
@@ -1,1 +1,20 @@
-'use strict'
+let loginModal = document.getElementById("login-modal");
+let mainContent = document.getElementById("main-content");
+let logoutBtn = document.getElementById("btn-logout")
+
+
+if (getFromStorage("LoginUser")) {
+  loginModal.style.display = "none";
+  document.getElementById("welcome-message").textContent = `Welcome ${
+    getFromStorage("LoginUser").fName
+  }`;
+} else {
+    mainContent.display = "none"
+}
+
+function logoutData() {
+    localStorage.removeItem("LoginUser")
+    location.href = "./pages/login.html"
+}
+
+logoutBtn.addEventListener("click", logoutData)
--- scripts/login.js
+++ scripts/login.js
@@ -1,26 +1,40 @@
-let userInput = document.getElementById("input-username")
-let passwordInput = document.getElementById("input-password")
-let submitBtn = document.getElementById("btn-submit")
+let userInput = document.getElementById("input-username");
+let passwordInput = document.getElementById("input-password");
+let submitBtn = document.getElementById("btn-submit");
 
-let userArr = getFromStorage("users").map(obj => parseUser(obj)) || []
+let userArr = getFromStorage("users").map((obj) => parseUser(obj)) || [];
 
 console.log("User arr: ", userArr);
 
 function validateLogin(data) {
-    if (!data.userName) {
-        alert("Please enter username!")
+  if (!data.userName) {
+    alert("Please enter username!");
+  }
+  if (!data.password) {
+    alert("Please enter password!");
+  }
+  if (!userArr.map((user) => user.userName).includes(data.userName)) {
+    alert("The user name is not registered!");
+  } else {
+    if (
+      userArr.filter((obj) => obj.userName == data.userName)[0].password !==
+      data.password
+    ) {
+      alert("Wrong password!");
+    } else {
+        saveToStorage("LoginUser", userArr.filter((obj) => obj.userName == data.userName)[0])
+        return true
     }
-    if (!data.password) {
-        alert("Please enter password!")
-    }
+  }
+  return false
 }
 
 function loginData() {
-    let user = {
-        userName: userInput.value,
-        password: passwordInput.value
-    }
-    validateLogin(user)
+  let user = {
+    userName: userInput.value,
+    password: passwordInput.value,
+  };
+  if (validateLogin(user)) location.href = "../index.html"
 }
 
-submitBtn.addEventListener("click", loginData)
+submitBtn.addEventListener("click", loginData);
